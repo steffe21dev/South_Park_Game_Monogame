fedccbff4101763cfd6c4db73509a3e725ac9d4b
steffe21dev <stefan.smudja0001@stud.hkr.se>
2020-03-05 22:38:56 +0100

    Add project files.

---
+++ DickGame/Character.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DickGame
+{
+    class Character
+    {
+       public Vector2 pos { get; set; } 
+       public int hp { get; set; }
+
+        public Character(Vector2 pos, int hp)
+        {
+            this.pos = pos;
+            this.hp = hp;
+        }
+    }
+}
---
+++ DickGame/Game1.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DickGame
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Character ship;
+        Texture2D ship_tex;
+
+        Texture2D background;
+        Rectangle mainframe;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            ship = new Character(new Vector2(350, 420), 100);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            background = Content.Load<Texture2D>("background");
+            ship_tex = Content.Load<Texture2D>("ship");
+
+
+            mainframe = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                ship.pos = new Vector2(ship.pos.X - 2f, ship.pos.Y);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                ship.pos = new Vector2(ship.pos.X + 2f, ship.pos.Y);
+            }
+
+
+            
+
+            // TODO: Add your update logic here
+
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            spriteBatch.Begin();
+
+
+            spriteBatch.Draw(background, mainframe, Color.White);
+
+
+            Rectangle destinationRectangle = new Rectangle((int)ship.pos.X, (int)ship.pos.Y, 50, 50);
+            spriteBatch.Draw(ship_tex,destinationRectangle, Color.White);
+
+
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
